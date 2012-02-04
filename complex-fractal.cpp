a5806b3ebe728425767b46fa1349281badadfe2d
Adam Auten <auty1395@gmail.com>
2012-02-03 20:35:56 -0600

    changed algorithm

--- complex-fractal.cpp
+++ complex-fractal.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <complex>
 #include <windows.h>
+
+
 using namespace std;
+
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 
-int fun1(double bound, int itlimit, double a, double b )
+char fun1(double bound, int itlimit, double a, double b, double ca, double cb)
 {
 
- int count = 0;
+ char count = 0;
 
   complex<double> comp (a,b);
 
 
   while(count <= itlimit)
   {
-      count++;
-      comp = comp * (comp + 4.0);
-      comp *= 5;
-      comp += 5;
+
+      comp = comp * comp;
+      comp +=  complex<double>(ca,cb);
+
+
+      //comp = comp * (comp );
+      //comp *= 8;
+
       if(comp.real() >= bound) break;
+      count++;
   }
 
   return count;
@@ -35,12 +43,12 @@ int main()
 {
 
   //declair variables
-  double ub = 0;
+  double ub = 0, ca = 0, cb = 0;
   int count=0;
   double a, b;
-  const int iterationlimit = 100000;
+  const int iterationlimit = 10000;
   complex<double> comp;
-  COORD maxwindowsize;
+  /*COORD maxwindowsize;
   maxwindowsize.X = 0;
   maxwindowsize.Y = 0;
   SMALL_RECT windowrect = {0,0,0,0};
@@ -58,26 +66,32 @@ int main()
   {
       cout << endl<< "Error setting window dimensions."<< endl;
   }
+*/
 
 
 
 
-
-
-  //run program
-  cout << "Welcome to the Complex Number lab.\n What is the upper bound?(any real number):";
-  cin >> ub;
-  cout << "Upper bound is "<< ub << endl;
-
-
-
-  for(double y = -15; y <= 15; y+=1) //31 rows, -15 to 15
+ while(1)
   {
-      for(double x = -15; x <=15; x+=1) //31 collumns, -15 to 15
+  //run program
+  cout << "Welcome to the Fractal Lab. /nFractals are generated with the formula Z = Z^2 + C, where C is the complex number (ca + cb * i)." << endl
+       << "This program draws the complex plane by printing the number of iterations taken for each point to meet the bound." << endl;
+  cout << "What is the bound?(any real number):";
+  cin >> ub;
+  cout << "What is ca?(any real number):";
+  cin >> ca;
+  cout << "What is cb?(any real number):";
+  cin >> cb;
+
+
+
+  for(double y = -5; y <= 5; y+=.02) //31 rows, -15 to 15
+  {
+      for(double x = -5; x <=5; x+=.02) //31 collumns, -15 to 15
       {
-          int j = fun1(ub, iterationlimit, x, y);
+          char j = fun1(ub, iterationlimit, x, y,ca, cb);
 
-
+          j = (j%25) + 'A';
           cout << hex << j << ""; // hex makes the console show numbers in hexadecimal format instead of decimal
 
 
@@ -85,7 +99,7 @@ int main()
       cout << endl;
   }
 
-
+  }
   return 0;
 
 
